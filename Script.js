e1c0750f30b60fa58e7261424e9171e517c090f6
Devashish pandey <152608759+Debashish9098@users.noreply.github.com>
2024-01-31 23:29:10 +0530

    Add files via upload
    it's my academy

---
+++ Script.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+                // document.addEventListener('DOMContentLoaded', function () {
+                //     document.addEventListener('click', function (event) {
+                //         hideAllDropdowns();
+                //     });
+        
+                //     function hideAllDropdowns() {
+                //         var dropdowns = document.querySelectorAll('.dropdown-content, .dropdown-content1, .dropdown-content2, .dropdown-content3, .dropdown-content4, .dropdown-content5');
+                //         dropdowns.forEach(function (dropdown) {
+                //             dropdown.style.display = 'none';
+                //         });
+                //     }
+        
+                //     function showDropdown(contentId) {
+                //         hideAllDropdowns();
+                //         document.getElementById(contentId).style.display = 'block';
+                //     }
+        
+                //     function hideDropdown(contentId) {
+                //         document.getElementById(contentId).style.display = 'none';
+                //     }
+                // });
+
+
+
+                
